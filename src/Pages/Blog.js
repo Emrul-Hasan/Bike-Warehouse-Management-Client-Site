9b42a13244f8d834268260df0039139f0b26be47
Emrul-Hasan <imrulhasan003@gmail.com>
2022-05-28 04:03:58 +0600

    Blog update

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -67,20 +67,47 @@ const Blog = () => {
 				<Accordion.Item eventKey="1">
 					<Accordion.Header>  When should we use Node.js and MongoDB?</Accordion.Header>
 					<Accordion.Body>
-						<p></p>
-					
+						<ul>
+							<li> We should use Nodejs</li>
+							<p>Any project needs a programming environment and a runtime library that offers you basic programming tools/support and can compile and/or interpret your code. Nodejs is such as tool for the Javascript programming language. There are other similar tools for other languages such as Python, Java, PHP, C#, C++, Go, etc...
+
+								So, if you want to write some kind of stand-alone program or server in Javascript, then you can use nodejs for it.</p>
+							<li>We should use MongoDB</li>
+							<p>If your application requires the ability to save data so that it can be effectively queried or updated later, you'll almost certainly need to utilize a database. There are a slew of well-known databases. One such database is MongoDB. Other databases include MariaDB, MySql, CouchDB, DynamoDB (on AWS), and Postgres. Distinct databases have different strengths (things they excel at) and new methods of being used, thus choosing the right/best database for what you're doing is a whole different topic.</p>
+						</ul>
+
 
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="2">
+					<Accordion.Header>  What's the purpose of JWT and how does it work??</Accordion.Header>
+					<Accordion.Body>
+						<p>JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.</p><br></br>
+						<p>JWTs are unique among web tokens in that they include a set of claims. Claims are a way for two parties to exchange information. The nature of these assertions is determined by the use case at hand. A claim could state who issued the token, how long it's valid, or what rights the client has been given.
+
+							A JWT is a three-part string separated by dots (.) that is serialized using base64.</p>
+
+
+					</Accordion.Body>
+				</Accordion.Item>
+				<Accordion.Item eventKey="3">
 					<Accordion.Header> Difference between SQL and NoSQL databases?</Accordion.Header>
 					<Accordion.Body>
-						<p> </p>
-						
+						<p>The critical differences between SQL vs NoSQL are: </p>
+						<ul>
+							<li>Relational databases are SQL, while non-relational databases are NoSQL.</li>
+							<li>SQL databases have a specified schema and employ structured query language.</li>
+							<li>For unstructured data, NoSQL databases use dynamic schemas.</li>
+							<li>SQL databases scale vertically, but NoSQL databases scale horizontally.</li>
+							<li>NoSQL databases are document, key-value, graph, or wide-column stores, whereas SQL databases are table-based.</li>
+							<li>SQL databases excel in multi-row transactions, while NoSQL excels at unstructured data such as documents and JSON.</li>
+						</ul>
+
 
 					</Accordion.Body>
 				</Accordion.Item>
 			</Accordion>
+
 		</div>
 	);
 
